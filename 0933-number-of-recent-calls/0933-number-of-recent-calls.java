4ec2e426b0dc445a4fd19e89976516dec634af85
sakthimurugan-24 <sakthia0607@gmail.com>
2026-08-22 13:42:13 +0530

    Time: 22 ms (51.99%), Space: 59.4 MB (67.59%) - LeetHub

---
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.java
@@ -0,0 +1,20 @@
+class RecentCounter {
+    Queue<Integer> q;
+    public RecentCounter() {
+        q=new LinkedList<>();
+    }
+    
+    public int ping(int t) {
+        while(!q.isEmpty() && q.peek()<t -3000) q.poll();
+
+        q.offer(t);
+
+        return q.size();
+    }
+}
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter obj = new RecentCounter();
+ * int param_1 = obj.ping(t);
+ */
